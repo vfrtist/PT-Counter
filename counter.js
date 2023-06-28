9998279082a670abd7d2b8bd060e85020791d432
vfrtist <137813892+vfrtist@users.noreply.github.com>
2023-06-28 18:04:38 -0400

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,6 +1,7 @@
 const counter = document.querySelector('.container')
 const endSet = document.querySelector('.end')
 const completeExercise = document.querySelector('.complete')
+const set = document.querySelector('.set').firstElementChild
 
 counter.addEventListener('click', () => {
     counter.firstElementChild.innerText++
@@ -9,10 +10,24 @@ counter.addEventListener('click', () => {
     }
 })
 
-endSet.addEventListener('click', setTo0)
-completeExercise.addEventListener('click', setTo0)
+endSet.addEventListener('click', () => {
+    set.innerText++;
+    setTo0()
+})
+
+completeExercise.addEventListener('click', () => {
+    set.innerText = '0';
+    setTo0()
+})
 
 function setTo0() {
     counter.firstElementChild.innerText = '0';
     counter.lastElementChild.classList.remove('hidden');
+    set.parentElement.animate(blink, { duration: 200, iterations: 1, })
 }
+
+const blink = [
+    { transform: 'scaleX(1)', color: 'black' },
+    { transform: 'scaleX(0)', color: '#f7fbff;' },
+    { transform: 'scaleX(1)', color: 'black' },
+]
